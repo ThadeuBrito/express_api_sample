6f969ef70a3102f1ae42a716dc1ca8d86d30be7d
Thadeu Brito <itvbrito@gmail.com>
2015-05-13 16:50:44 -0300

    Added routes

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-app.listen(5000, function(){
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.listen(3000, function(){
   console.log('The server ir running in the port 5000');
 });
 
 app.get('/', function(req, res) {
   res.send("<h1>Hello World!</h1>");
 })
+
+app.get('/users', function(req, res) {
+})
+
+app.post('/users', function(res, req) {
+});
+
+app.get('/users/:id', function(res, req) {
+});
+
+app.put('/users/:id', function(res, req) {
+});
+
+app.delete('/users/:id', function(res, req) {
+});
