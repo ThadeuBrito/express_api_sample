a439e4605b0e8c65c04862bad6c4fe4fdf3f41fa
Thadeu Brito <itvbrito@gmail.com>
2015-05-15 23:24:38 -0300

    Mongo db config

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var db_string = "mongodb://127.0.0.1/express_api_sample";
+var mongoose = require('mongoose').connect(db_string);
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting!'));
+
+db.once('open', function() {
+  console.log('Mongo db connected!');
+});
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -9,7 +19,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.listen(3000, function(){
-  console.log('The server ir running in the port 5000');
+  console.log('The server ir running in the port 3000');
 });
 
 app.get('/', function(req, res) {
