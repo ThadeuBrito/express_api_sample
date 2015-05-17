3d0cb807e63709f5eb1b6ca2ff46160a3c066813
Thadeu Brito <itvbrito@gmail.com>
2015-05-17 17:12:03 -0300

    Added method Update and Delete in userController

--- api/controllers/user_controller.js
+++ api/controllers/user_controller.js
@@ -17,3 +17,35 @@ exports.show = function(params, callback) {
       callback(user);
   });
 };
+
+exports.update = function(req, callback) {
+  db.User.findById(req.params.id, function(error, user) {
+    if (req.body.name)
+      user.name = req.body.name;
+
+    if (req.body.email)
+      user.email = req.body.email;
+
+    user.updated_at = new Date;
+
+    user.save(function(error, user) {
+      if (error)
+        callback(error);
+      else
+        callback(user);
+    });
+  });
+}
+
+exports.delete = function(params, callback) {
+  db.User.findById(params.id, function(error, user) {
+    if (error) {
+      callback(error);
+    } else {
+      user.remove(function(error) {
+        if (!error)
+          callback('User successfully removed!');
+      });
+    }
+  });
+}
--- config/routes.js
+++ config/routes.js
@@ -33,37 +33,14 @@ module.exports = function(app) {
   });
 
   app.put('/users/:id', function(req, res) {
-    var id = req.params.id;
-    db.User.findById(id, function(error, user) {
-      if (req.body.name)
-        user.name = req.body.name;
-
-      if (req.body.email)
-        user.email = req.body.email;
-
-      user.updated_at = new Date;
-
-      user.save(function(error, user) {
-        if (error)
-          res.json(error);
-        else
-          res.json(user);
-      });
-
-    })
+    userController.update(req, function(response) {
+      res.json(response);
+    });
   });
 
   app.delete('/users/:id', function(req, res) {
-    var id = req.params.id;
-    db.User.findById(id, function(error, user) {
-      if (error) {
-        res.json(error);
-      } else {
-        user.remove(function(error) {
-          if (!error)
-            res.json('User successfully removed!')
-        });
-      }
+    userController.delete(req.params, function(response) {
+      res.json(response);
     });
   });
 }
